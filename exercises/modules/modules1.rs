bc9372650ad1c9e6e0917b69b4c30a011901ba1d
Orestis <orestis.malaspinas@pm.me>
2023-08-18 00:16:52 +0200

    translation of modules

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,12 +1,12 @@
 // modules1.rs
 //
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
-// hint.
+// Exécutez `rustlings hint modules1` ou utilisez la sous-commande `hint`
+// de `watch` pour obtenir une indication.
 
 // I AM NOT DONE
 
 mod sausage_factory {
-    // Don't let anybody outside of this module see this!
+    // Ne laissez personne en dehors de ce module voir ceci !
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
